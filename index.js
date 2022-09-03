d005cf9a9f20e2ac0f54d61158b5d2e37fce6853
Abigail Arisa <abigailarisa1@gmail.com>
2022-09-03 17:11:25 -0500

    commit tree 42db44ef149bc5577be0e277343c8299f8bae1e7

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+// const friends =["Guadalupe", "Ollie", "Aki"]
+// let event = surprise
+
+function writeCards(friends, event){
+    let thankYouCard = []
+    for(let i = 0; i < friends.length; i++ ) {
+    thankYouCard.push(`Thank you, ${friends[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankYouCard
+}
+
+function countDown(){
+let i = 11
+while(i > 0 )
+console.log(i-=1)
+}
